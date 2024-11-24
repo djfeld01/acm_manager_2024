e959ee9c3b1109c6a47327d9d1501bc7c33e56fc
David Feldman <djfeld01@gmail.com>
2024-11-24 15:03:57 -0500

    updated some formatting

--- src/components/EmployeeCommittedPayroll.tsx
+++ src/components/EmployeeCommittedPayroll.tsx
@@ -7,6 +7,7 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "./ui/collapsible";
+import { CardContent, CardHeader, CardTitle } from "./ui/card";
 
 type EmployeeCommittedPayrollProps = {
   employee: UserWithActivities;
@@ -27,41 +28,44 @@ function EmployeeCommittedPayroll({
   const [isOpen, setIsOpen] = useState(false);
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <div>
-        <div className="grid grid-cols-3 bg-grey-300">
-          <div>Commission</div>
-          <div>Vacation</div>
-          <div>Miles</div>
-          <CollapsibleTrigger asChild>
-            <div className="cursor-pointer">
-              ${(committedCommission || 0).toFixed(2)}
-            </div>
-          </CollapsibleTrigger>
-        </div>
-        <CollapsibleContent>
-          {committedActivities.map((activity) => (
-            <div key={activity.activityId}>
-              <div>
-                {activity.unitName}{" "}
-                {new Date(activity.date).toLocaleDateString(undefined, {
-                  month: "2-digit",
-                  day: "2-digit",
-                })}
+      <div className="bg-gray-200 rounded-3xl">
+        <CardHeader className="p-1">Committed For This Payroll</CardHeader>
+        <CardContent>
+          <div className="grid grid-cols-3 bg-grey-300">
+            <div className="font-semibold">Commission</div>
+            <div className="font-semibold">Vacation</div>
+            <div className="font-semibold">Miles</div>
+            <CollapsibleTrigger asChild>
+              <div className="cursor-pointer">
+                ${(committedCommission || 0).toFixed(2)}
               </div>
-              <Button
-                onClick={() =>
-                  updateActivities("uncommitActivity", [activity.activityId])
-                }
-                variant="ghost"
-                size="sm"
-                className="w-6 h-6 p-0 flex items-center justify-center"
-              >
-                <CircleMinus className="h-4 w-4" />
-                <span className="sr-only">Toggle</span>
-              </Button>
-            </div>
-          ))}
-        </CollapsibleContent>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            {committedActivities.map((activity) => (
+              <div key={activity.activityId}>
+                <div>
+                  {activity.unitName}{" "}
+                  {new Date(activity.date).toLocaleDateString(undefined, {
+                    month: "2-digit",
+                    day: "2-digit",
+                  })}
+                </div>
+                <Button
+                  onClick={() =>
+                    updateActivities("uncommitActivity", [activity.activityId])
+                  }
+                  variant="ghost"
+                  size="sm"
+                  className="w-6 h-6 p-0 flex items-center justify-center"
+                >
+                  <CircleMinus className="h-4 w-4" />
+                  <span className="sr-only">Toggle</span>
+                </Button>
+              </div>
+            ))}
+          </CollapsibleContent>
+        </CardContent>
       </div>
     </Collapsible>
   );
